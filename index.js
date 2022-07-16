076c5ef66172dc3b4ae69f016c092148fa225ecf
Jacob <jacobmunden@gmail.com>
2022-07-16 17:20:17 -0600

    commit tree 03705f5c09f7c2163216ab84ef3d5298c88b2488

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+ 
+/* Attempt 1
+function totalBatteries(batteryBatches) {
+    const countedBatteries = batteryBatches.reduce(function(accumulator, element){
+        return element += accumulator
+    })
+    return countedBatteries;
+}
+
+console.log(totalBatteries);
+*/
+
+const totalBatteries = batteryBatches.reduce((accumulator, number) => {
+    return accumulator + number;
+}, 0);
